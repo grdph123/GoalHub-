d5eaf91e68601156ccd8b66e76b6a8341ab9f66d
Garuga Dewangga Putra Handikto <garugadph@gmail.com>
2025-09-10 01:14:22 +0700

    feat: add GoalHub name to views and template

--- main/views.py
+++ main/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
-        'npm' : '2406437615',
+        'store_name': 'GoalHub',   # tambahan nama toko
+        'npm': '2406437615',
         'name': 'Garuga Dewangga Putra Handikto',
         'class': 'PBP F'
     }
 
     return render(request, "main.html", context)
-
